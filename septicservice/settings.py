c386fdd52fd7d347f6e24278bb88068df6b92b0c
Mike Maven <mike.maven@aboutgolf.com>
2023-02-15 18:41:24 -0500

    Update application to use JWT authentication on the endpoint
    - add JWT auth as a valid authentication method on the project

--- septicservice/settings.py
+++ septicservice/settings.py
@@ -49,6 +49,14 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+    ]
+}
+
 ROOT_URLCONF = 'septicservice.urls'
 
 TEMPLATES = [
--- septicserviceapp/views.py
+++ septicserviceapp/views.py
@@ -6,32 +6,39 @@ from django.shortcuts import render
 
 # Create your views here.
 import requests
-from django.http import JsonResponse, HttpResponseServerError
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework import status
 
 
-def getSepticInfoByAddress(request, address, zip_code):
-    # Replace spaces in address with +
-    address = address.replace(' ', '+')
+class SepticInfoView(APIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
 
-    # Make request to HouseCanary API to retrieve property details
-    api_key = os.environ.get('HOUSECANARY_API_KEY')
-    # Get URL from app configuration
-    config = apps.get_app_config('septicserviceapp')
-    url = config.house_canary_property_details_api
-    params = {'address': address, 'zipcode': zip_code}
-    query_string = urllib.parse.urlencode(params)
-    full_url = f"{url}?{query_string}"
-    headers = {'Authorization': f'Bearer {api_key}'}
-    response = requests.get(full_url, headers=headers)
+    def get(self, request, address, zip_code):
+        # Replace spaces in address with +
+        address = address.replace(' ', '+')
 
-    # Parse response and check for septic system
-    if response.ok:
-        data = response.json()
-        sewer = data['property/details/summary']['sewer']
-        has_septic = sewer.lower() == 'septic'
-    else:
-        return HttpResponseServerError()
+        # Make request to HouseCanary API to retrieve property details
+        api_key = os.environ.get('HOUSECANARY_API_KEY')
+        # Get URL from app configuration
+        config = apps.get_app_config('septicserviceapp')
+        url = config.house_canary_property_details_api
+        params = {'address': address, 'zipcode': zip_code}
+        query_string = urllib.parse.urlencode(params)
+        full_url = f"{url}?{query_string}"
+        headers = {'Authorization': f'Bearer {api_key}'}
+        response = requests.get(full_url, headers=headers)
 
-    # Return boolean indicating whether property has septic system
-    septic_info = {'has_septic': has_septic}
-    return JsonResponse(septic_info)
+        # Parse response and check for septic system
+        if response.ok:
+            data = response.json()
+            sewer = data['property/details/summary']['sewer']
+            has_septic = sewer.lower() == 'septic'
+            # Return boolean indicating whether property has septic system
+            septic_info = {'has_septic': has_septic}
+            return Response(septic_info)
+        else:
+            return Response(status=status.HTTP_500_INTERNAL_SERVER_ERROR)
